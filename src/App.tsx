379ab197d43353a8100e2a665dbc5fcb901e09dd
TohirDev <isakulovtohir902@gmail.com>
2024-01-26 10:07:58 +0500

    `App.css` file deleted and `index` file codes deleted

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
 import LoginPage from "./pages/auth/login";
 import RegistrationPage from "./pages/auth/registeration";
-import HomePage from "./pages/home";
+import Home from "./pages/home";
 import PageNotFound from "./pages/404";
 import MainLayout from "./pages/layout";
 
@@ -10,7 +9,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
-        <Route path="/" element={<HomePage />} index />
+        <Route path="/" element={<Home />} index />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegistrationPage />} />
         <Route path="*" element={<PageNotFound />} />
