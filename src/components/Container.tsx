d0668f169b8f52bb507c7c1ae68fb3c420838a52
TohirDev <isakulovtohir902@gmail.com>
2024-01-26 10:32:35 +0500

    `TEST:` Container component

---
+++ src/components/Container.tsx
@@ -0,0 +1,17 @@
+import { Box } from "@mui/material"
+import { ReactNode } from "react"
+
+type TContainer = {
+    children: ReactNode,
+    style?: Record<string, unknown>
+}
+
+const Container = ({children, style}: TContainer) => {
+    return (
+        <Box component={'div'} sx={{margin: '0 auto', width: '1350px', maxWidth: '1350px', ...style}}>
+            {children}
+        </Box>
+    )
+}
+
+export default Container
