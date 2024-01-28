9b2bf3342c4b81b68101b86c0efa653e3ac08dfb
TohirDev <isakulovtohir902@gmail.com>
2024-01-28 18:58:00 +0500

    UPDATED: home page and admin panel

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,12 +1,24 @@
 import Navbar from "../../components/Navbar";
+import { getUserToken } from "../../global";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const data = getUserToken();
+  const navigate = useNavigate();
+
   return (
     <div>
       <Navbar />
-      <h1>
-        hieee
-      </h1>
+      <h1>hieee</h1>
+
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => navigate("/login")}
+      >
+        {data ? "Profil" : "E'lon berish"}
+      </Button>
     </div>
   );
 };
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,19 +1,9 @@
-import { Button } from "@mui/material";
-import { useNavigate } from "react-router-dom";
 import HomePage from "./HomePage";
 
 const Home = () => {
-  const navigate = useNavigate();
   return (
     <>
       <HomePage />
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => navigate("/login")}
-      >
-        Go to Login
-      </Button>
     </>
   );
 };
