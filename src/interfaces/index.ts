c8a3304f461046253a1f69ba9b2f6d7ea19f5cfa
username-shakha <username.shakha@gmail.com>
2024-02-04 22:26:10 +0500

    FINISH: created ProductCard && finished ProductList

---
+++ src/interfaces/index.ts
@@ -0,0 +1,30 @@
+export interface IProduct {
+  _id: string;
+  Image: {
+    _id: string;
+    public_id: string;
+    url: string;
+  }[];
+  status: string;
+  model_name: string;
+  display: string;
+  cpu: string;
+  gpu: string;
+  ram: string;
+  memory: string;
+  battery: string;
+  box: boolean;
+  condition: string;
+  exchange: boolean;
+  location: string;
+  links: [string, string];
+  phone_number: string;
+  __v: number;
+}
+
+export interface IData {
+  products: IProduct[];
+  page: number;
+  pages: number;
+  total_products: number;
+}
