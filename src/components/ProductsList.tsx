73b807a5893f3551b30ed0df07da58a4d907e8ec
username-shakha <username.shakha@gmail.com>
2024-02-04 19:56:45 +0500

    create productsList components

---
+++ src/components/ProductsList.tsx
@@ -0,0 +1,10 @@
+import { Box, Container } from "@mui/system";
+
+const ProductsList = () => {
+  return (
+    <Box component={"section"}>
+      <Container maxWidth="lg"></Container>
+    </Box>
+  );
+};
+export default ProductsList;
