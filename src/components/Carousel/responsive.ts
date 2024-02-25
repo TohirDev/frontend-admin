23224f00b79ad311e76e45ccb4a478b11ace88f7
username-shakha <username.shakha@gmail.com>
2024-02-25 21:48:08 +0500

    created product card

---
+++ src/components/Carousel/responsive.ts
@@ -0,0 +1,19 @@
+export const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 3000, min: 1150 },
+    items: 6,
+  },
+  desktop: {
+    breakpoint: { max: 1150, min: 810 },
+    items: 4.5,
+  },
+  tablet: {
+    breakpoint: { max: 810, min: 675 },
+    items: 3.5,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 400 },
+    items: 2,
+  },
+};
--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -1,56 +1,121 @@
-import { useState } from "react";
-import {
-  Box,
-  Container,
-  Grid,
-  Pagination,
-  Stack,
-  Typography,
-} from "@mui/material";
-import { IData } from "../interfaces";
-import useFetch from "../hooks/useFetch";
-import ProductCard from "./ProductCard";
+//hook
+// import useFetch from "../hooks/useFetch";
+
+//Api Data
+// import { IData } from "../interfaces";
+
+//mui
+import { Box, Container, Stack, Link, Typography } from "@mui/material";
+
+//react-multi-carousel package
+import CarouselReact from "./Carousel";
+
+//components
+import ShoppingCard from "./Shop/ShoppingCard";
+
+// data
+import { products } from "./Shop/ShoppingCard/constants";
+
+import { ziplogo } from "../assets";
 
 const ProductsList = () => {
-  const [page, setPage] = useState(1);
-  const { data, loading, error } = useFetch<IData>(
-    `https://laptop-uz.onrender.com/api/product?page=${page}`
-  );
-  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
-    console.log(event); //event
-    setPage(value);
-  };
+  // const { data, loading, error } = useFetch<IData>(
+  //   `https://laptop-uz.onrender.com/api/product?page=${page}`
+  // );
+  //console.log(data, loading, error)
 
   return (
-    <Box component={"section"} bgcolor={"#e0e0e0"} py={3}>
-      <Container maxWidth="xl">
-        <Typography variant="h3" py={2}>
-          Laptops
-        </Typography>
+    <>
+      <Container
+        maxWidth="xl"
+        sx={{
+          height: { xs: "fit-content", md: "450px" },
+          mt: 4,
+          px: 0,
+        }}
+      >
+        <Stack
+          sx={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
 
-        {loading && <Typography variant="h1">...Loading</Typography>}
-        {error && <Typography variant="h1">{error.message}</Typography>}
-        {/* 
-        {data?.products?.map((product) => (
-          
-        ))} */}
-        <Grid
-          container
-          spacing={{ xs: 2, md: 3 }}
-          columns={{ xs: 4, sm: 8, md: 12 }}
+            px: { xs: "8px", sm: "20px" },
+            mb: { xs: "-10px", md: "-43px" },
+          }}
         >
-          {data?.products?.map((laptop, i) => (
-            <Grid item xs={2} sm={4} md={2} key={i}>
-              <ProductCard key={laptop._id} product={laptop} />
-            </Grid>
-          ))}
-        </Grid>
-        <Stack spacing={2}>
-          <Typography>Page: {page}</Typography>
-          <Pagination count={data?.pages} page={page} onChange={handleChange} />
+          <Typography
+            variant="h6"
+            component={"h2"}
+            sx={{
+              color: "black",
+              fontFamily: "Poppins, sans-serif",
+              lineHeight: "33px",
+              fontWeight: 600,
+              fontSize: "22px",
+            }}
+          >
+            New Products
+          </Typography>
+          <Link
+            href={"#"}
+            sx={{
+              fontSize: "13px",
+              fontFamily: "Poppins, sans-serif",
+              color: "#0156FF",
+            }}
+          >
+            See All New Products
+          </Link>
         </Stack>
+
+        <CarouselReact>
+          {products.map((product) => (
+            <ShoppingCard key={product.id} data={product} />
+          ))}
+        </CarouselReact>
       </Container>
-    </Box>
+
+      <Box
+        sx={{
+          height: "70px",
+          display: "flex",
+          alignItems: "center",
+          px: "25px",
+          justifyContent: "center",
+          bgcolor: "#F5F7FF",
+          mb: "35px",
+        }}
+      >
+        <img src={ziplogo} alt="ziplogo" />
+
+        <Typography
+          sx={{
+            color: "#272560",
+            fontFamily: "Poppins",
+            fontSize: "14px",
+            fontWeight: 400,
+            pl: "10px",
+            ml: "10px",
+            borderLeft: "2px solid #00AEB8",
+          }}
+        >
+          <strong>own</strong> it now, up to 6 months interest free
+          <Box
+            component={"span"}
+            sx={{
+              textDecoration: "underline",
+              fontSize: "13px",
+              pl: "5px",
+              cursor: "pointer",
+            }}
+          >
+            learn more
+          </Box>
+        </Typography>
+      </Box>
+    </>
   );
 };
+
 export default ProductsList;
