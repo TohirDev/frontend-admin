c8676bfbe0118044a779d0897d2b1545b4f5b4fb
TohirDev <isakulovtohir902@gmail.com>
2024-03-15 06:26:25 +0500

    updated: elonlar, setting, sidebar pages

--- src/pages/admin/components/elonlar/components/active.tsx
+++ src/pages/admin/components/elonlar/components/active.tsx
@@ -1,5 +1,15 @@
+import { Button } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
 function Active() {
-  return <div>Active</div>;
+  return (
+    <div>
+      <h1>active</h1>
+      <NavLink to={"/"}>
+        <Button variant="contained">go to home page</Button>
+      </NavLink>
+    </div>
+  );
 }
 
 export default Active;
--- src/pages/admin/components/elonlar/index.tsx
+++ src/pages/admin/components/elonlar/index.tsx
@@ -1,15 +1,9 @@
-import { Box, Typography } from "@mui/material";
-import SideNav from "../sidebar";
+import { Box } from "@mui/material";
 import { useState } from "react";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Active from "./components/active";
-
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
-}
+import { CustomTabPanel } from "./components/CustomTabPanel";
 
 function Elonlar() {
   const [value, setValue] = useState(0);
@@ -29,54 +23,29 @@ function Elonlar() {
   }
 
   return (
-    <Box sx={{ display: "flex" }}>
-      <SideNav />
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <Box sx={{ width: "100%" }}>
-          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-            <Tabs
-              value={value}
-              onChange={handleChange}
-              aria-label="basic tabs example"
-            >
-              <Tab key={0} label="Item One" {...a11yProps(0)} />
-              <Tab key={1} label="Item Two" {...a11yProps(1)} />
-              <Tab key={2} label="Item Three" {...a11yProps(2)} />
-            </Tabs>
-          </Box>
-          <CustomTabPanel value={value} index={0}>
-            <Active />
-          </CustomTabPanel>
-          <CustomTabPanel value={value} index={1}>
-            Item Two
-          </CustomTabPanel>
-          <CustomTabPanel value={value} index={2}>
-            Item Three
-          </CustomTabPanel>
-        </Box>
+    <Box sx={{ width: "100%" }}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+        >
+          <Tab key={0} label="Item One" {...a11yProps(0)} />
+          <Tab key={1} label="Item Two" {...a11yProps(1)} />
+          <Tab key={2} label="Item Three" {...a11yProps(2)} />
+        </Tabs>
       </Box>
+      <CustomTabPanel value={value} index={0}>
+        <Active />
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        Item Two
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={2}>
+        Item Three
+      </CustomTabPanel>
     </Box>
   );
 }
 
 export default Elonlar;
-
-function CustomTabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
