cde07baa54e12038b2912524e47f866d65f5aaf8
TohirDev <isakulovtohir902@gmail.com>
2024-03-19 11:12:02 +0500

    feat: breadcrumbs added, table created

--- src/pages/admin/components/elonlar/index.tsx
+++ src/pages/admin/components/elonlar/index.tsx
@@ -1,51 +1,81 @@
-import { Box } from "@mui/material";
-import { useState } from "react";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import Active from "./components/active";
-import { CustomTabPanel } from "./components/CustomTabPanel";
+import {
+  Box,
+  Button,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import { BreadCrumb } from "../../../../components/BreadCrumb";
+import { useFetchData } from "@/hooks/useFetch";
+import { useEffect, useState } from "react";
+import { TDataType } from "@/types";
+import { AddElon } from "./components/AddElon";
 
-function Elonlar() {
-  const [value, setValue] = useState(0);
-
-  const handleChange = (
-    _event: React.SyntheticEvent,
-    newValue: number
-  ) => {
-    setValue(newValue);
-  };
-
-  function a11yProps(index: number) {
-    return {
-      id: `simple-tab-${index}`,
-      "aria-controls": `simple-tabpanel-${index}`,
-    };
-  }
-
+export const Elonlar = () => {
+  const [getElonlar, { data, loading, error }] = useFetchData<TDataType>(
+    "https://laptop-uz.onrender.com/api/product/get-all"
+  );
+  const [open, setOpen] = useState(false);
+  useEffect(() => {
+    getElonlar();
+  }, [getElonlar]);
+  if (error) return <h1>Error get the fuck out of here</h1>;
+  if (loading) return <h1>Loading...</h1>;
   return (
-    <Box sx={{ width: "100%" }}>
-      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          aria-label="basic tabs example"
-        >
-          <Tab key={0} label="Item One" {...a11yProps(0)} />
-          <Tab key={1} label="Item Two" {...a11yProps(1)} />
-          <Tab key={2} label="Item Three" {...a11yProps(2)} />
-        </Tabs>
+    <Box>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <BreadCrumb
+          key={1}
+          title="E'lonlar"
+          subtitleOne="Toza Laptop"
+          subtitleTwo="Laptop"
+          subtitleOnePath="/"
+          subtitleTwoPath="/admin/elonlar"
+        />
+        <Button variant="contained" onClick={() => setOpen(true)}>
+          Add Product
+        </Button>
       </Box>
-      <CustomTabPanel value={value} index={0}>
-        <Active />
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={1}>
-        Item Two
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={2}>
-        Item Three
-      </CustomTabPanel>
+      <AddElon width={613} onClose={() => setOpen(false)} open={open} />
+      <TableContainer component={Paper} sx={{ mt: 4 }}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Dessert (100g serving)</TableCell>
+              <TableCell align="right">Calories</TableCell>
+              <TableCell align="right">Fat&nbsp;(g)</TableCell>
+              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data?.data.map((item) => (
+              <TableRow
+                key={item._id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {item.display}
+                </TableCell>
+                <TableCell align="right">{item.battery}</TableCell>
+                <TableCell align="right">{item.gpu}</TableCell>
+                <TableCell align="right">{item.location}</TableCell>
+                <TableCell align="right">{item.phone_number}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Box>
   );
-}
-
-export default Elonlar;
+};
