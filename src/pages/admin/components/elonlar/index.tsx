a2a5690a20c30bf8db347cd5623e6e0e0e8b749a
TohirDev <isakulovtohir902@gmail.com>
2024-04-21 22:05:31 +0500

    feat: elonlar table updated

--- src/pages/admin/components/elonlar/index.tsx
+++ src/pages/admin/components/elonlar/index.tsx
@@ -1,21 +1,28 @@
 import { useFetchData } from "@/hooks/useFetch";
 import { useEffect, useState } from "react";
 import { TDataType } from "@/types";
+import {
+  Pagination,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 
 export const Elonlar = () => {
   const [getElonlar, { data, loading, error }] = useFetchData<TDataType>(
     "https://laptop-uz.onrender.com/api/product/get-all"
   );
   const [currentPage, setCurrentPage] = useState(1);
-  const [recordsPerPage, setReacordsPerPage] = useState("5");
+  const [recordsPerPage, setReacordsPerPage] = useState("20");
   const lastIndex = currentPage * Number(recordsPerPage);
   const firstIndex = lastIndex - Number(recordsPerPage);
   const records = data?.data.slice(firstIndex, lastIndex);
   let npage = 0;
-  let numbers: number[] = [];
   if (data?.count !== undefined) {
     npage = Math.ceil(data?.count / Number(recordsPerPage));
-    numbers = [...Array(npage + 1).keys()].slice(1);
   }
   useEffect(() => {
     getElonlar();
@@ -23,66 +30,49 @@ export const Elonlar = () => {
   if (error) return <h1>Error get the fuck out of here</h1>;
   if (loading) return <h1>Loading...</h1>;
 
-  function nextPage() {
-    if (currentPage != lastIndex) {
-      setCurrentPage(currentPage + 1);
-    }
-  }
-
-  function prePage() {
-    if (currentPage != firstIndex) {
-      setCurrentPage(currentPage - 1);
-    }
-  }
-
   function changeCPage(id: number) {
     setCurrentPage(id);
   }
   return (
     <div>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {records?.map((el) => (
-            <tr key={el._id}>
-              <td>{el._id}</td>
-              <td>{el.model_name}</td>
-              <td>{el.gpu}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <ul style={{ display: "flex", listStyle: "none", padding: 0 }}>
-        <li>
-          <button onClick={prePage} disabled={currentPage === 1}>
-            Previous
-          </button>
-        </li>
-        {numbers.map((el) => (
-          <li key={el}>
-            <button
-              onClick={() => changeCPage(el)}
-              style={{
-                background: el === currentPage ? "red" : "white",
-              }}
-            >
-              {el}
-            </button>
-          </li>
-        ))}
-        <li>
-          <button onClick={nextPage} disabled={currentPage === npage}>
-            Next
-          </button>
-        </li>
-      </ul>
-      <select
+      <TableContainer>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Dessert (100g serving)</TableCell>
+              <TableCell align="right">Calories</TableCell>
+              <TableCell align="right">Fat&nbsp;(g)</TableCell>
+              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {records?.map((row) => (
+              <TableRow
+                key={row._id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row._id}
+                </TableCell>
+                <TableCell align="right">{row.battery}</TableCell>
+                <TableCell align="right">{row.condition}</TableCell>
+                <TableCell align="right">{row.location}</TableCell>
+                <TableCell align="right">{row.model_name}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <Pagination
+          count={npage}
+          shape="rounded"
+          variant="outlined"
+          onChange={(_e, value) => changeCPage(value)}
+          page={currentPage}
+        ></Pagination>
+      </TableContainer>
+
+      {/* <select
         name="page"
         onChange={(e) => setReacordsPerPage(e.target.value)}
       >
@@ -90,7 +80,7 @@ export const Elonlar = () => {
         <option value={10}>10</option>
         <option value={15}>15</option>
         <option value={20}>20</option>
-      </select>
+      </select> */}
     </div>
   );
 };
